9e13aa1deec1c49331a15885a878d60dac9e07f7
Nikita Golovin <golovin.nik@gmail.com>
2016-10-09 16:28:22 +0300

    some fixes

--- build/dev-client.js
+++ build/dev-client.js
@@ -1,9 +1,7 @@
-/* eslint-disable */
-require('eventsource-polyfill')
-var hotClient = require('webpack-hot-middleware/client?noInfo=true&reload=true')
+require('eventsource-polyfill');
 
-hotClient.subscribe(function (event) {
-  if (event.action === 'reload') {
-    window.location.reload()
-  }
-})
+let hotClient = require('webpack-hot-middleware/client?noInfo=true&reload=true');
+hotClient.subscribe(event => {
+  if (event.action === 'reload')
+    window.location.reload();
+});
--- build/dev-server.js
+++ build/dev-server.js
@@ -12,6 +12,9 @@ let port = process.env.PORT || 8080;
 let app = express();
 let compiler = webpack(webpackConfig);
 
+// handle fallback for HTML5 history API
+app.use(require('connect-history-api-fallback')());
+
 let devMiddleware = require('webpack-dev-middleware')(compiler, {
   publicPath: webpackConfig.output.publicPath,
   stats: {
@@ -20,25 +23,18 @@ let devMiddleware = require('webpack-dev-middleware')(compiler, {
   }
 });
 
+app.use(devMiddleware);
+
 let hotMiddleware = require('webpack-hot-middleware')(compiler);
+
 // force page reload when html-webpack-plugin template changes
-/*
 compiler.plugin('compilation', compilation => {
   compilation.plugin('html-webpack-plugin-after-emit', (data, cb) => {
     hotMiddleware.publish({action: 'reload'});
     cb();
   })
 });
-*/
 
-// handle fallback for HTML5 history API
-app.use(require('connect-history-api-fallback')());
-
-// serve webpack bundle output
-app.use(devMiddleware);
-
-// enable hot-reload and state-preserving
-// compilation error display
 app.use(hotMiddleware);
 
 // serve pure static assets
@@ -58,5 +54,5 @@ module.exports = app.listen(port, err => {
   }
   let uri = 'http://localhost:' + port;
   console.log('Listening at ' + uri + '\n');
-  opn(uri);
+  //opn(uri);
 });
