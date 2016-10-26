be8f33f7d0fb8a86ad0c33fb3ba8fa5db8a8161e
Tigran Tumasov <shugar348@gmail.com>
2016-10-26 18:45:02 +0300

    karaoke section

--- src/components/Download.vue
+++ src/components/Download.vue
@@ -2,7 +2,7 @@
   .download
     .chasez
     .one-hit
-      | #OneHitWonder
+      | #1HitWonder
       .socials
         a.fb
         a.twitter
@@ -12,7 +12,7 @@
       .title
         .title-image
         .title-inner
-          | Go to the opening night of Opening Night! <span>*woah*</span>
+          | Sing a Karaoke Duet with a Broadway Star!
 
       .steps
         .steps-item
@@ -28,12 +28,12 @@
           .steps-point
             | 2.
           .steps-text
-            | Record a Video with JC under the ‘Opening Night’ Section
+            | Record a duet to <span>"I Know What Boys Like" with Lesli Margherita</span>
         .steps-item
           .steps-point
             | 3.
           .steps-text
-            | Post to Facebook, Twitter or Instagram with the hashtag #OneHitWonder and <span>WIN</span>!
+            | Post to Facebook, Twitter or Instagram with the hashtag <span>#1HitWonder</span> and you will be entered to win $100 itunes gift card!
 </template>
 
 <script>
@@ -87,6 +87,7 @@
       height: 100%;
       position: absolute;
       left: 0;
+      bottom: 0;
     }
 
     .title {
